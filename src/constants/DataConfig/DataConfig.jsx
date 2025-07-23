45351b89e4550698dabea08a18b5906ab45cf6e5
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-22 23:04:34 -0300

    nodo "DataConfig.jsx" criado para facilitar criação de UI

---
+++ src/constants/DataConfig/DataConfig.jsx
@@ -0,0 +1,37 @@
+export const DataConfig = [
+  {
+    id: "perfil",
+    title: "Perfil",
+    description: "Gerencie suas informações de login em um só lugar.",
+    alert: "",
+    link: "/inicio/configuracoes/perfil",
+  },
+  {
+    id: "temas",
+    title: "Temas",
+    description: "Altere o visual com temas variados.",
+    alert: "",
+    link: "/inicio/configuracoes/temas",
+  },
+  {
+    id: "lixeira",
+    title: "Lixeira",
+    description: "Salve Rotinas apagadas separadas das ativas.",
+    alert: "",
+    link: "/inicio/configuracoes/lixeira",
+  },
+  {
+    id: "extras",
+    title: "Funções estras",
+    description: "Apague Rotinas, lixeira e dados de login.",
+    alert: "",
+    link: "",
+  },
+  {
+    id: "limpar",
+    title: "Limpar dados",
+    description: "Apague Rotinas, lixeira e dados de login.",
+    alert: "",
+    link: "/inicio/configuracoes/limpar-dados",
+  },
+];
