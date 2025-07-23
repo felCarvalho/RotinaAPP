863622d4128bb6e7cb72c947378cb575bb7d2bf4
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-23 17:13:50 -0300

    aplicando props de style em "headerContent.jsx" para melhor personalização

--- src/component/headerContent.jsx
+++ src/component/headerContent.jsx
@@ -2,27 +2,36 @@ import { Button } from "./btn";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { H3 } from "./subTitle";
 
-export function HeaderContent({ iconBack, title, iconClosed, className, btnBack, btnClosed }) {
+export function HeaderContent({
+  iconBack,
+  title,
+  iconClosed,
+  classNameHeader,
+  classNameBtn,
+  classNameTitle,
+  btnBack,
+  btnClosed,
+}) {
   return (
-    <header className={`${className} flex flex-row justify-between`}>
+    <header className={`${classNameHeader} flex flex-row justify-between`}>
       <div className="flex flex-row items-center gap-2">
         <Button
           onClick={btnBack}
           type="button"
           whileTap={{ scale: 0.9 }}
-          className="min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100"
+          className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
           ariaLabel="Voltar"
         >
           <FontAwesomeIcon icon={iconBack} />
         </Button>
-        <H3 title={title} className="!text-lg font-medium text-blue-400" />
+        <H3 title={title} className={`${classNameTitle} !text-lg font-medium text-blue-400`} />
       </div>
       <div>
         <Button
           whileTap={{ scale: 0.9 }}
           onClick={btnClosed}
           type="button"
-          className="min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100"
+          className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
           ariaLabel="Fechar"
         >
           <FontAwesomeIcon icon={iconClosed} />
