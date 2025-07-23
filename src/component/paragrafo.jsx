ea7d8db666de75d882fa5fffb69c8a8b0e695431
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-22 22:58:24 -0300

    mudanças pontuais

--- src/component/paragrafo.jsx
+++ src/component/paragrafo.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export function P({ children, title, className, ...props }) {
   return (
-    <motion.p {...props} className={`${className} font-medium text-sm tracking-wide text-shadow-blue-50 text-shadow-xs`}>
+    <motion.p {...props} className={`${className} text-sm font-medium tracking-wide text-shadow-blue-50 text-shadow-xs`}>
       {children}
       {title}
     </motion.p>
