25dc2b04392c39c96babe19507e170b34eb4ee64
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-27 14:38:50 -0300

    apliquei a classe 'py'ao inves de 'pt e pb' no 'btn.jsx'.

--- src/component/btn.jsx
+++ src/component/btn.jsx
@@ -20,7 +20,7 @@ export function Button({
       layoutId={layoutId}
       id={id}
       aria-label={ariaLabel}
-      className={`${className} cursor-pointer rounded-full border-none bg-blue-400 px-3 pt-1.5 pb-1.5 text-base text-white shadow-md shadow-blue-50 outline-2 outline-offset-4 outline-transparent transition-discrete will-change-auto focus:outline-blue-200 active:bg-blue-200`}
+      className={`${className} cursor-pointer rounded-full border-none bg-blue-400 px-3 py-1.5 text-base text-white shadow-md shadow-blue-50 outline-2 outline-offset-4 outline-transparent transition-discrete will-change-auto focus:outline-blue-200 active:bg-blue-200`}
     >
       {children}
     </motion.button>
