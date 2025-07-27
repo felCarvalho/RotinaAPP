f68b2ff6cf182e3d22774636ee85c2e25c5648ac
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-27 15:14:34 -0300

    modicações: usei componente 'P.jsx' para aplicar pre estilização \ adições: rota de index:true para mostra o aviso de erro para mobile e desktop com as devidas props deles sendo utilizadas.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -7,12 +7,19 @@ import { Perfil } from "../pages/Perfil/Perfil";
 import { Temas } from "../pages/Temas/Temas";
 import { LimparDados } from "../pages/LimparDados/LimparDados";
 import { Lixeira } from "../pages/Lixeira/Lixeira";
+import { P } from "../component/paragrafo";
+import { LayoutConfigError } from "../layout/LayoutConfig/LayoutConfigError/LayoutConfigError";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <div className="text-amber-400"> estamos verificando o erro</div>,
+    errorElement: (
+      <div className="my-72 w-full text-center text-2xl text-amber-400">
+        <P title="Estamos verificando o erro" />
+      </div>
+    ),
     children: [
       {
         index: true,
@@ -30,6 +37,16 @@ const routes = createBrowserRouter([
             path: "configuracoes",
             element: <LayoutConfig />,
             children: [
+              {
+                index: true,
+                element: (
+                  <LayoutConfigError
+                    mobile="Volte a tela de configurações clicando no btn abaixo!"
+                    desktop="Escolha uma confguração para ver os detalhes aqui!"
+                    icon={faArrowUp}
+                  />
+                ),
+              },
               {
                 path: "perfil",
                 element: <Perfil />,
