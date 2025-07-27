a128f30a5a7f58f6a767188d8f5cd11be3d91c73
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-27 14:40:36 -0300

    aplicando 'selectNone' para impedir de marcar texto em telas que usam 'overlay.jsx1'.

--- src/component/overlay.jsx
+++ src/component/overlay.jsx
@@ -4,7 +4,7 @@ export function Overlay({ children, className, ...props }) {
   return (
     <motion.div
       {...props}
-      className={`${className} fixed top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center backdrop-blur-3xl`}
+      className={`${className} fixed top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center backdrop-blur-3xl select-none`}
     >
       {children}
     </motion.div>
