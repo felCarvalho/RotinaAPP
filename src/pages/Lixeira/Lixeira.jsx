05c02dfe25bf0c6b9439043f1d7443da957a72ae
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-27 15:12:06 -0300

    adições: apenas um copy do jsx de 'Perfil.jsx' para efeitos de teste da UI/UX.

--- src/pages/Lixeira/Lixeira.jsx
+++ src/pages/Lixeira/Lixeira.jsx
@@ -1,3 +1,32 @@
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+//import { Button } from "../../component/btn";
+import { HeaderContent } from "../../component/headerContent";
+//import { Input } from "../../component/input";
+//import { H3 } from "../../component/subTitle";
+import { useNavigate, useOutletContext } from "react-router";
+
 export function Lixeira() {
-  return "sem conteúdo...";
+  const navigate = useNavigate();
+  const { setLayout } = useOutletContext();
+
+  return (
+    <div className="z-50 h-full rounded-[50px] bg-blue-50 p-5 shadow-sm shadow-blue-50">
+      <HeaderContent
+        title="Perfil"
+        iconBack={faAngleLeft}
+        iconClosed={faX}
+        btnBack={() => {
+          setLayout({ isMobileLayout: false });
+          navigate("/inicio/configuracoes");
+        }}
+        btnClosed={() => {
+          setLayout({ isMobileLayout: false });
+          navigate("/inicio/configuracoes");
+        }}
+        classNameBtn="bg-white !text-blue-400"
+      />
+      <div className="py-8"></div>
+    </div>
+  );
 }
