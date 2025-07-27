8aa7db50cfda1c96d086cb969ccb5a71e98d4836
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-27 15:06:19 -0300

    modificações: removi dependencias não usada, \ adições: crie typeConfigToggle para aplicar um btn extras com link personalizado para redirecionamento, ele já retorna o jsx pronto e estilizado.

--- src/pages/Configuracao/Config.jsx
+++ src/pages/Configuracao/Config.jsx
@@ -1,19 +1,20 @@
-import { faAngleLeft, faX, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft, faX, faTrash, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { P } from "../../component/paragrafo";
 import { HeaderContent } from "../../component/headerContent";
 import { H3 } from "../../component/subTitle";
-import { Button } from "../../component/btn";
 import { Toggle } from "../../component/toggle";
+import { Button } from "../../component/btn";
 import { DataConfig } from "../../constants/DataConfig/DataConfig";
 import { RotinaStore } from "../../store/UseRotina";
-import { useNavigate } from "react-router";
+import { useNavigate, NavLink } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { NavLink } from "react-router";
+import { LayoutStore } from "../../store/UseLayout";
 
 export function Config() {
+  //
+  const { setLayout } = LayoutStore();
   //estado global e função para atualizar o mesmo
   const { statusFunction, setStatusFunction } = RotinaStore();
-
   //usado para navegação
   const navigate = useNavigate();
 
@@ -30,49 +31,83 @@ export function Config() {
     }
   }
 
+  function typeConfigToggle({ chave, link }) {
+    switch (chave) {
+      case "lixeira":
+        return (
+          <Button onClick={() => navigate(link)} className="flex flex-row items-center justify-center gap-2 !py-1">
+            <i>
+              <FontAwesomeIcon icon={faTrash} />
+            </i>
+            <P title="Lixeira" />
+          </Button>
+        );
+
+      default:
+        "";
+    }
+  }
+
   return (
-    <div className="h-full rounded-[50px] bg-white p-5 shadow-sm shadow-blue-50">
+    <div className="h-full rounded-[50px] bg-white shadow-sm shadow-blue-50">
       <HeaderContent
-        btnBack={() => navigate(-1)}
-        btnClosed={() => navigate("/inicio")}
+        btnBack={() => {
+          navigate(-1);
+          setLayout({ isMobileLayout: true });
+        }}
+        btnClosed={() => {
+          navigate("/inicio");
+          setLayout({ isMobileLayout: true });
+        }}
         title="Configurações"
         iconBack={faAngleLeft}
         iconClosed={faX}
+        classNameHeader="p-5"
       />
-      <div className="box-border overflow-auto pt-8">
+      <div className="box-border h-[300px] overflow-auto px-5">
         {DataConfig.map((c) => (
           <div
             key={c?.id}
-            className="mb-3 flex flex-row items-center justify-between rounded-full bg-blue-100/5 p-5 shadow-sm shadow-blue-50"
+            className="my-3 flex flex-row items-center justify-between rounded-2xl bg-blue-100/5 px-3 py-4 shadow-sm shadow-blue-50"
             onDoubleClick={() => {
               verificarConfigStatus({ chave: c?.id }) && setStatusFunction({ [c?.id]: !statusFunction?.[c?.id] });
             }}
           >
-            <NavLink>
-              <H3 title={c?.title} className="cursor-pointer text-base text-blue-400" />
-              <P title={c?.description} className="w-40 truncate font-light whitespace-nowrap text-blue-300" />
-            </NavLink>
             {!verificarConfigStatus({ chave: c?.id }) ? (
-              <div>
-                <Button
-                  onClick={() => {
-                    setTimeout(() => {
-                      navigate(c?.link);
-                    }, 300);
-                  }}
-                  className="min-h-10 min-w-10 cursor-pointer rounded-full bg-white !p-0 shadow-xs shadow-blue-100"
-                >
+              <NavLink
+                onClick={() => setLayout({ isMobileLayout: true })}
+                to={c?.link}
+                className={({ isActive }) =>
+                  !isActive
+                    ? "flex w-full flex-row items-center justify-center text-blue-300"
+                    : "flex w-full flex-row text-blue-400"
+                }
+              >
+                <div className="w-full">
+                  <H3 title={c?.title} className="cursor-pointer text-base" />
+                  <P title={c?.description} className="w-40 truncate font-light whitespace-nowrap text-blue-300" />
+                </div>
+                <Button type="button" className="min-h-10 min-w-10 bg-white">
                   <i className="text-blue-400">
                     <FontAwesomeIcon icon={faAngleRight} />
                   </i>
                 </Button>
-              </div>
+              </NavLink>
             ) : (
-              <Toggle
-                id={c?.id}
-                boleano={statusFunction?.[c?.id]}
-                setBoleano={(e) => setStatusFunction({ [c?.id]: e.target.checked })}
-              />
+              <div className="justify-cente flex w-full flex-col items-center gap-3">
+                <div className="flex w-full flex-row items-center">
+                  <div className="w-full">
+                    <H3 title={c?.title} className="cursor-pointer text-base text-blue-400" />
+                    <P title={c?.description} className="w-40 truncate font-light whitespace-nowrap text-blue-300" />
+                  </div>
+                  <Toggle
+                    id={c?.id}
+                    boleano={statusFunction?.[c?.id]}
+                    setBoleano={(e) => setStatusFunction({ [c?.id]: e.target.checked })}
+                  />
+                </div>
+                <div className="">{typeConfigToggle({ chave: c?.id, link: c?.link })}</div>
+              </div>
             )}
           </div>
         ))}
