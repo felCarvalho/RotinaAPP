310a4eabbc61efb8635aa523616024038275713e
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-29 23:54:46 -0300

    adição: aplicando uma nova estrutura para o header ser flutuante

--- src/component/headerContent.jsx
+++ src/component/headerContent.jsx
@@ -13,29 +13,31 @@ export function HeaderContent({
   btnClosed,
 }) {
   return (
-    <header className={`${classNameHeader} flex flex-row justify-between`}>
-      <div className="flex flex-row items-center gap-2">
-        <Button
-          onClick={btnBack}
-          type="button"
-          whileTap={{ scale: 0.9 }}
-          className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
-          ariaLabel="Voltar"
-        >
-          <FontAwesomeIcon icon={iconBack} />
-        </Button>
-        <H3 title={title} className={`${classNameTitle} !text-lg font-medium text-blue-400`} />
-      </div>
-      <div>
-        <Button
-          whileTap={{ scale: 0.9 }}
-          onClick={btnClosed}
-          type="button"
-          className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
-          ariaLabel="Fechar"
-        >
-          <FontAwesomeIcon icon={iconClosed} />
-        </Button>
+    <header className={`${classNameHeader} relative`}>
+      <div className="absolute right-0 left-0 z-10 flex flex-row justify-between">
+        <div className="flex flex-row items-center gap-2">
+          <Button
+            onClick={btnBack}
+            type="button"
+            whileTap={{ scale: 0.9 }}
+            className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
+            ariaLabel="Voltar"
+          >
+            <FontAwesomeIcon icon={iconBack} />
+          </Button>
+          <H3 title={title} className={`${classNameTitle} !text-lg font-medium text-blue-400`} />
+        </div>
+        <div className="">
+          <Button
+            whileTap={{ scale: 0.9 }}
+            onClick={btnClosed}
+            type="button"
+            className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
+            ariaLabel="Fechar"
+          >
+            <FontAwesomeIcon icon={iconClosed} />
+          </Button>
+        </div>
       </div>
     </header>
   );
