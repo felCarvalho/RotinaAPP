76431ff19575b0328cd618b674e9685411e0fc9a
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-07-30 00:02:14 -0300

    modificação: mudanças nas condições de render e remoção de dependencias

--- src/layout/LayoutConfig/LayoutConfig.jsx
+++ src/layout/LayoutConfig/LayoutConfig.jsx
@@ -5,7 +5,7 @@ import { Button } from "../../component/btn";
 import { Overlay } from "../../component/overlay";
 import { useResizeView } from "../../hooks/UseResizeView";
 import { LayoutStore } from "../../store/UseLayout";
-import { useEffect, useCallback } from "react";
+import { useCallback } from "react";
 import { motion } from "framer-motion";
 import { Outlet } from "react-router";
 
@@ -18,17 +18,13 @@ export function LayoutConfig() {
 
   const { verificarWidth } = useResizeView();
 
-  useEffect(() => {
-    localStorage.setItem("isLayout", JSON.stringify(isLayout));
-  }, [isLayout]);
-
   return (
     <Overlay
       initial={{ y: 500, scale: 0, opacity: 0 }}
       animate={{ y: 0, scale: 1, opacity: 1 }}
       transition={{ type: "spring", stiffness: 150, damping: 15, mass: 1, bounce: 0.3 }}
     >
-      {verificarWidth({ largura: 800 }) && (
+      {verificarWidth({ largura: 1000 }) && (
         <div className={`flex h-full w-full items-center !justify-start gap-5 p-5 ${layout()}`}>
           <div className="h-full w-full">
             <Config setLayout={{ setLayout, isLayout }} />
@@ -43,11 +39,11 @@ export function LayoutConfig() {
           <div className="h-full w-full">{<Outlet context={{ setLayout, isLayout }} /> ?? "conteudo zero"}</div>
         </div>
       )}
-      {!verificarWidth({ largura: 799 }) && (
-        <div className="flex h-full w-full items-center !justify-start gap-5 p-5">
+      {!verificarWidth({ largura: 1000 }) && (
+        <div className="flex h-full w-full items-center !justify-start gap-10 p-5">
           <motion.div
             initial={{ y: 0 }}
-            animate={isLayout?.isMobileLayout ? { y: -700 } : { y: 0 }}
+            animate={isLayout?.isMobileLayout ? { y: -1100 } : { y: 0 }}
             transition={{ type: "spring", stiffness: 100 }}
             className="h-full w-full"
           >
@@ -55,7 +51,7 @@ export function LayoutConfig() {
           </motion.div>
           {isLayout?.isMobileLayout && (
             <motion.div
-              initial={{ y: 700 }}
+              initial={{ y: 1100 }}
               animate={{ y: 0 }}
               transition={{ type: "spring", stiffness: 100 }}
               className="absolute top-0 right-0 bottom-0 left-0 m-5 rounded-[50px] bg-white shadow-md shadow-blue-50"
