beb30e60d3979314b7f0ca2b7092e417d4f01a60
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-08-06 14:09:26 -0300

    modificação: render condicional de icon, uso opcional de funções e criação de class para personalizar cada btn individualmente

--- src/component/headerContent.tsx
+++ src/component/headerContent.tsx
@@ -4,15 +4,17 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { H3 } from "./subTitle";
 
 interface HeaderContentType {
-  iconBack: IconProp;
-  iconClosed: IconProp;
+  iconBack: IconProp | null;
+  iconClosed: IconProp | null;
   title: string;
   classNameHeader?: string;
   classNameBtn?: string;
+  classNameBtnBack?: string;
+  classNameBtnClosed?: string;
   classNameTitle?: string;
   classNameHeaderDiv?: string;
-  btnBack: () => void;
-  btnClosed: () => void;
+  btnBack?: () => void;
+  btnClosed?: () => void;
 }
 
 export function HeaderContent({
@@ -21,33 +23,31 @@ export function HeaderContent({
   iconClosed,
   classNameHeader,
   classNameBtn,
+  classNameBtnBack,
+  classNameBtnClosed,
   classNameTitle,
-  classNameHeaderDiv,
   btnBack,
   btnClosed,
 }: HeaderContentType) {
   return (
     <header className={`${classNameHeader} relative`}>
-      <div
-        className={`absolute right-0 left-0 z-10 flex flex-row justify-between rounded-t-[50px] p-5 backdrop-blur-3xl ${classNameHeaderDiv}`}
-      >
+      <div className="absolute top-0 right-0 left-0 flex flex-row justify-between rounded-t-[50px] p-5 backdrop-blur-3xl">
         <div className="flex flex-row items-center gap-2">
           <Button
             onClick={btnBack}
             type="button"
-            className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
+            className={`${classNameBtn} ${classNameBtnBack} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
             ariaLabel="Voltar"
           >
             <FontAwesomeIcon icon={iconBack} />
           </Button>
-          +
           <H3 children={null} title={title} className={`${classNameTitle} !text-lg font-medium text-blue-400`} />
         </div>
         <div className="">
           <Button
             onClick={btnClosed}
             type="button"
-            className={`${classNameBtn} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
+            className={`${classNameBtn} ${classNameBtnClosed} min-h-11 min-w-11 rounded-full bg-blue-400 !p-0 text-white shadow-2xl shadow-blue-100`}
             ariaLabel="Fechar"
           >
             <FontAwesomeIcon icon={iconClosed} />
