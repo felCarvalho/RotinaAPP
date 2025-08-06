db0004134a4e6c3d7505bb08e9682694b0509944
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-08-06 14:21:23 -0300

    modificação: retirando btn de closed

--- src/pages/LimparDados/LimparDados.tsx
+++ src/pages/LimparDados/LimparDados.tsx
@@ -1,5 +1,5 @@
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faX, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 //import { Button } from "../../component/btn";
 import { HeaderContent } from "../../component/headerContent";
 //import { Input } from "../../component/input";
@@ -12,22 +12,20 @@ export function LimparDados() {
   const { setLayout, isLayout } = LayoutStore();
 
   return (
-    <div className="z-50 h-full rounded-[50px] bg-blue-50 shadow-sm shadow-blue-50">
+    <div className="z-50 h-full rounded-[50px] bg-blue-50 pb-5 shadow-sm shadow-blue-50">
       <HeaderContent
         title="Limpar Dados"
         iconBack={faAngleLeft}
-        iconClosed={faX}
+        iconClosed={null}
         btnBack={() => {
-          setLayout({ isMobileLayout: true, isDesktopLayout: isLayout?.isDesktopLayout });
-          navigate("/inicio/configuracoes");
-        }}
-        btnClosed={() => {
           setLayout({ isMobileLayout: false, isDesktopLayout: isLayout?.isDesktopLayout });
           navigate("/inicio/configuracoes");
         }}
+        btnClosed={undefined}
         classNameBtn="bg-white !text-blue-400"
+        classNameBtnClosed="!min-w-0 !min-h-0 bg-transparent"
       />
-      <div></div>
+      <div className="scroll-hide h-full overflow-auto rounded-[50px] px-3.5 pt-22"></div>
     </div>
   );
 }
