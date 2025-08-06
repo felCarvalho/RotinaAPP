f0c0d9bc50cc184ee812753b287754cae6e4a916
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-08-06 14:19:03 -0300

    modificação: adicioando a função par manipulação de layout  no retorno de typeConfigToogle(), usando useEffec() para fazer toda a manipulção manual com base na URL

--- src/pages/Configuracao/Config.tsx
+++ src/pages/Configuracao/Config.tsx
@@ -6,9 +6,10 @@ import { Toggle } from "../../component/toggle";
 import { Button } from "../../component/btn";
 import { DataConfig } from "../../constants/DataConfig/DataConfig";
 import { RotinaStore } from "../../store/UseRotina";
-import { useNavigate, NavLink } from "react-router";
+import { useNavigate, NavLink, useLocation } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { LayoutStore } from "../../store/UseLayout";
+import { useEffect } from "react";
 
 export function Config() {
   const { setLayout, isLayout } = LayoutStore();
@@ -16,6 +17,7 @@ export function Config() {
   const { statusFunction, setStatusFunction } = RotinaStore();
   //usado para navegação
   const navigate = useNavigate();
+  const location = useLocation();
 
   //verificando quais config devem ter btn toggle ou não
   function verificarConfigStatus({ chave }: { chave: string }) {
@@ -34,7 +36,14 @@ export function Config() {
     switch (chave) {
       case "lixeira":
         return (
-          <Button type="button" onClick={() => navigate(link)} className="flex flex-row items-center justify-center gap-2 !py-1">
+          <Button
+            type="button"
+            onClick={() => {
+              setLayout({ isMobileLayout: true, isDesktopLayout: isLayout?.isDesktopLayout });
+              navigate(link);
+            }}
+            className="flex flex-row items-center justify-center gap-2 !py-1"
+          >
             <i>
               <FontAwesomeIcon icon={faTrash} />
             </i>
@@ -46,8 +55,21 @@ export function Config() {
     }
   }
 
+  useEffect(() => {
+    const route = location.pathname === "/inicio/configuracoes";
+    const linkRoute = DataConfig.find((c) => c?.link === location.pathname);
+
+    if (route) {
+      setLayout({ isMobileLayout: false, isDesktopLayout: isLayout?.isDesktopLayout });
+    }
+
+    if (!route && linkRoute) {
+      setLayout({ isMobileLayout: true, isDesktopLayout: isLayout?.isDesktopLayout });
+    }
+  }, [location]);
+
   return (
-    <div className="h-full rounded-[50px] bg-white shadow-sm shadow-blue-50">
+    <div className="h-full rounded-[50px] bg-white pb-5 shadow-sm shadow-blue-50">
       <HeaderContent
         btnBack={() => {
           navigate(-1);
@@ -55,61 +77,60 @@ export function Config() {
         }}
         btnClosed={() => {
           navigate("/inicio");
-          setLayout({ isMobileLayout: true, isDesktopLayout: isLayout?.isDesktopLayout });
+          setLayout({ isMobileLayout: false, isDesktopLayout: isLayout?.isDesktopLayout });
         }}
         title="Configurações"
         iconBack={faAngleLeft}
         iconClosed={faX}
+        classNameHeader=""
       />
-      <div className="px-3.5 py-5">
-        <div className="scroll-bar box-border h-90 overflow-y-auto pt-16">
-          {DataConfig.map((c) => (
-            <div
-              key={c?.id}
-              className="mx-1.5 my-3 flex flex-row items-center justify-between rounded-2xl bg-blue-100/5 px-3 py-4 shadow-sm shadow-blue-50"
-              onDoubleClick={() =>
-                verificarConfigStatus({ chave: c?.id }) && setStatusFunction({ [c?.id]: statusFunction?.[c?.id] })
-              }
-            >
-              {!verificarConfigStatus({ chave: c?.id }) ? (
-                <NavLink
-                  onClick={() => setLayout({ isMobileLayout: true, isDesktopLayout: isLayout?.isDesktopLayout })}
-                  to={c?.link}
-                  className={({ isActive }) =>
-                    !isActive
-                      ? "flex w-full flex-row items-center justify-center text-blue-300"
-                      : "flex w-full flex-row text-blue-400"
-                  }
-                >
+      <div className="scroll-hide h-full overflow-auto rounded-[50px] px-3.5 pt-22">
+        {DataConfig.map((c) => (
+          <div
+            key={c?.id}
+            className="mx-1.5 my-3 flex flex-row items-center justify-between rounded-2xl bg-blue-100/5 px-3 py-4 shadow-sm shadow-blue-50"
+            onDoubleClick={() =>
+              verificarConfigStatus({ chave: c?.id }) && setStatusFunction({ [c?.id]: statusFunction?.[c?.id] })
+            }
+          >
+            {!verificarConfigStatus({ chave: c?.id }) ? (
+              <NavLink
+                onClick={() => setLayout({ isMobileLayout: true, isDesktopLayout: isLayout?.isDesktopLayout })}
+                to={c?.link}
+                className={({ isActive }) =>
+                  !isActive
+                    ? "flex w-full flex-row items-center justify-center text-blue-300"
+                    : "flex w-full flex-row text-blue-400"
+                }
+              >
+                <div className="w-full">
+                  <H3 title={c?.title} className="cursor-pointer text-base" />
+                  <P title={c?.description} className="w-40 truncate font-light whitespace-nowrap text-blue-300" />
+                </div>
+                <Button type="button" className="min-h-10 min-w-10 bg-white">
+                  <i className="text-blue-400">
+                    <FontAwesomeIcon icon={faAngleRight} />
+                  </i>
+                </Button>
+              </NavLink>
+            ) : (
+              <div className="justify-cente flex w-full flex-col items-center gap-3">
+                <div className="flex w-full flex-row items-center">
                   <div className="w-full">
-                    <H3 title={c?.title} className="cursor-pointer text-base" />
+                    <H3 title={c?.title} className="cursor-pointer text-base text-blue-400" />
                     <P title={c?.description} className="w-40 truncate font-light whitespace-nowrap text-blue-300" />
                   </div>
-                  <Button type="button" className="min-h-10 min-w-10 bg-white">
-                    <i className="text-blue-400">
-                      <FontAwesomeIcon icon={faAngleRight} />
-                    </i>
-                  </Button>
-                </NavLink>
-              ) : (
-                <div className="justify-cente flex w-full flex-col items-center gap-3">
-                  <div className="flex w-full flex-row items-center">
-                    <div className="w-full">
-                      <H3 title={c?.title} className="cursor-pointer text-base text-blue-400" />
-                      <P title={c?.description} className="w-40 truncate font-light whitespace-nowrap text-blue-300" />
-                    </div>
-                    <Toggle
-                      name={c?.id}
-                      boleano={statusFunction?.[c?.id]}
-                      setBoleano={(e) => setStatusFunction({ [c?.id]: e.target.checked })}
-                    />
-                  </div>
-                  <div className="">{typeConfigToggle({ chave: c?.id, link: c?.link })}</div>
+                  <Toggle
+                    name={c?.id}
+                    boleano={statusFunction?.[c?.id]}
+                    setBoleano={(e) => setStatusFunction({ [c?.id]: e.target.checked })}
+                  />
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
+                <div className="">{typeConfigToggle({ chave: c?.id, link: c?.link })}</div>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
