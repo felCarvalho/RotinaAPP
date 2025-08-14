a7d8ecaae6e4a7d8e7400aaccfb08dcecda81292
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-08-14 09:36:17 -0300

    modificação: apenas teste para implementação de animação

--- src/component/radio.tsx
+++ src/component/radio.tsx
@@ -26,7 +26,9 @@ export function Radio({ children, id, checked, onChange, ariaLabel, classNameRad
         aria-label={ariaLabel}
         className={`${classNameRadio} peer h-6 w-6 appearance-none rounded-full border-1 border-blue-100 bg-white outline-0 outline-offset-4 focus:outline-2 focus:outline-blue-50`}
       />
-      {children ?? <span className="absolute top-1 left-1 h-4 w-4 rounded-full peer-checked:bg-blue-400"></span>}
+      {children ?? (
+        <motion.span className="absolute top-1 left-1 h-4 w-4 rounded-full peer-checked:bg-blue-400"></motion.span>
+      )}
     </motion.label>
   );
 }
