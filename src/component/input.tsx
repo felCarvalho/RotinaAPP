a7db0c5833781156f3480c93791479ad1f00d75e
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-08-14 09:34:00 -0300

    modificação: apliquei um opacity no shadow do btn/ apliquei um fontMedium para teste.

--- src/component/input.tsx
+++ src/component/input.tsx
@@ -32,7 +32,7 @@ export function Input({
       onChange={onChange}
       whileTap={{ scale: 0.9, stitchTiles: 300 }}
       transition={{ type: "spring" }}
-      className={`${className} w-full rounded-full border border-blue-50 p-2.5 indent-1.5 text-base text-blue-900 shadow-sm shadow-blue-50 outline-offset-4 transition-discrete will-change-auto placeholder:text-sm placeholder:text-blue-300 focus:bg-blue-50 focus:shadow-none focus:outline-blue-50`}
+      className={`${className} w-full rounded-full border border-blue-50 p-2.5 indent-1.5 text-base font-medium text-blue-900 shadow-sm shadow-blue-50/50 outline-offset-4 transition-discrete will-change-auto placeholder:text-sm placeholder:text-blue-300 focus:bg-blue-50 focus:shadow-none focus:outline-blue-50`}
       value={value}
       placeholder={placeholder}
       disabled={status}
