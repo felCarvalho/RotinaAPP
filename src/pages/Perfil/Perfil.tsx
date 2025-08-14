87d921e5522f1e611e602a0e9ab9accb2cccf4c0
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2025-08-14 09:58:24 -0300

    modificação: retirando amostragem de dados de input para texto normal e já aplicando reticências onde foi necessario

--- src/pages/Perfil/Perfil.tsx
+++ src/pages/Perfil/Perfil.tsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "../../component/btn";
 import { HeaderContent } from "../../component/headerContent";
-import { Input } from "../../component/input";
 import { H3 } from "../../component/subTitle";
 import { P } from "../../component/paragrafo";
 import { LayoutStore } from "../../store/UseLayout";
@@ -26,19 +25,27 @@ export function Perfil() {
         classNameBtn="bg-white !text-blue-400"
         classNameBtnClosed="!min-w-0 !min-h-0 bg-transparent"
       />
-      <div className="scroll-hide h-full overflow-auto rounded-[50px] px-3.5 pt-22">
-        <div className="flex w-full flex-col items-start justify-start gap-2 px-1">
+      <div className="scrol-hide mx-5 h-full overflow-auto rounded-3xl pt-25">
+        <div className="mb-5 flex w-full flex-col items-start justify-start gap-2 rounded-3xl">
           <H3 title="Dados Pessoais" className="text-blue-400" />
           <div className="flex w-full flex-col items-start justify-start gap-5 rounded-2xl bg-white p-4">
-            <label className="relative flex w-full flex-col gap-2">
+            <div className="flex flex-row items-center justify-center gap-2">
               <P title="Nome:" className="text-blue-400" />
-              <Input placeholder="" type="text" className="!rounded-2xl !shadow-none" />
-            </label>
+              <H3 title="nome de teste por enquanto" className="xs:max-2xs:w-36 3xs:max-4xs:w-50 md:w-60 truncate text-blue-300" />
+            </div>
+            <div className="flex flex-row items-center justify-center gap-2">
+              <P title="Email:" className="text-blue-400" />
+              <H3 title="email de teste por enquanto" className="xs:max-2xs:w-36 3xs:max-4xs:w-50 md:w-60 truncate text-blue-300" />
+            </div>
+            <div className="flex flex-row items-center justify-center gap-2">
+              <P title="Email:" className="text-blue-400" />
+              <H3 title="senha de teste por enquanto" className="xs:max-2xs:w-36 3xs:max-4xs:w-50 md:w-60 truncate text-blue-300" />
+            </div>
           </div>
         </div>
-        <div className="flex flex-col items-start justify-start gap-2 px-1">
+        <div className="my-2 flex flex-col items-start justify-start gap-2">
           <H3 title="Conta" className="text-blue-400" />
-          <div className="flex w-full flex-col items-start gap-2 rounded-2xl bg-white p-4">
+          <div className="flex w-full flex-col items-start gap-2 rounded-3xl bg-white p-4">
             <label className="flex flex-row items-center justify-center gap-2">
               <P title="Sair:" className="text-blue-400" />
               <div>
@@ -66,6 +73,14 @@ export function Perfil() {
                   className="flex flex-row items-center justify-center gap-2 !bg-red-400 !shadow-red-50 focus:!shadow-red-50 focus:!outline-red-400"
                 >
                   <P title="Deletar" />
+                </Button>
+              </div>
+            </label>
+            <label className="flex flex-row items-center justify-center gap-2">
+              <P title="Alterar dados:" className="text-blue-400" />
+              <div>
+                <Button type="button" className="flex flex-row items-center justify-center gap-2">
+                  <P title="Alterar dados pessoais" className="w-20 truncate sm:w-full" />
                 </Button>
               </div>
             </label>
