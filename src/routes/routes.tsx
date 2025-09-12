5f116b9edd1665910642fac9054c90ae0a3c545c
Felipe Carvalho <felipecarvalho@gmail.com>
2025-09-12 01:35:01 -0300

    modiicação: alterando rota de config de filha, para pai, retirando impors não usados.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,21 +1,27 @@
-import { createBrowserRouter, redirect } from "react-router";
-import Layout from "../layout/LayoutPrincipal/Layout";
-import { LayoutMain } from "../layout/LayoutMain/LayoutMain";
-import { SearchTasks } from "../pages/SearchTasks/SearchTasks";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { createBrowserRouter } from "react-router";
+import { P } from "../component/paragrafo";
+import { GuardLogin } from "../guards/GuardsLogin/GuardLogin";
 import { LayoutConfig } from "../layout/LayoutConfig/LayoutConfig";
-import { Perfil } from "../pages/Perfil/Perfil";
-import { Temas } from "../pages/Temas/Temas";
+import { LayoutConfigError } from "../layout/LayoutConfig/LayoutConfigError/LayoutConfigError";
+import { LayoutMain } from "../layout/LayoutMain/LayoutMain";
+import Layout from "../layout/LayoutPrincipal/Layout";
+import { CriarConta } from "../pages/CriarConta/CriarConta";
 import { LimparDados } from "../pages/LimparDados/LimparDados";
 import { Lixeira } from "../pages/Lixeira/Lixeira";
-import { P } from "../component/paragrafo";
 import { Login } from "../pages/Login/Login";
-import { LayoutConfigError } from "../layout/LayoutConfig/LayoutConfigError/LayoutConfigError";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { Perfil } from "../pages/Perfil/Perfil";
+import { SearchTasks } from "../pages/SearchTasks/SearchTasks";
+import { Temas } from "../pages/Temas/Temas";
 
 const routes = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/criar-conta",
+    element: <CriarConta />,
   },
   {
     path: "/",
@@ -28,7 +34,7 @@ const routes = createBrowserRouter([
     children: [
       {
         index: true,
-        loader: () => redirect("/inicio"),
+        loader: GuardLogin,
       },
       {
         path: "inicio",
@@ -38,39 +44,39 @@ const routes = createBrowserRouter([
             path: "buscar",
             element: <SearchTasks />,
           },
-          {
-            path: "configuracoes",
-            element: <LayoutConfig />,
-            children: [
-              {
-                index: true,
-                element: (
-                  <LayoutConfigError
-                    desktop="Selecione alguma configuração para ver mais detalhes dela aqui"
-                    mobile="Aperte no btn abaixo para ir para pagina de configurações"
-                    icon={faArrowUp}
-                  />
-                ),
-              },
-              {
-                path: "perfil",
-                element: <Perfil />,
-              },
-              {
-                path: "temas",
-                element: <Temas />,
-              },
-              {
-                path: "lixeira",
-                element: <Lixeira />,
-              },
-              {
-                path: "limpar-dados",
-                element: <LimparDados />,
-              },
-            ],
-          },
         ],
+      },
+    ],
+  },
+  {
+    path: "configuracoes",
+    element: <LayoutConfig />,
+    children: [
+      {
+        index: true,
+        element: (
+          <LayoutConfigError
+            desktop="Selecione alguma configuração para ver mais detalhes dela aqui"
+            mobile="Aperte no btn abaixo para ir para pagina de configurações"
+            icon={faArrowUp}
+          />
+        ),
+      },
+      {
+        path: "perfil",
+        element: <Perfil />,
+      },
+      {
+        path: "temas",
+        element: <Temas />,
+      },
+      {
+        path: "lixeira",
+        element: <Lixeira />,
+      },
+      {
+        path: "limpar-dados",
+        element: <LimparDados />,
       },
     ],
   },
