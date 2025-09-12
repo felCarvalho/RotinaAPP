bb9aaedd521aaf58e008f4fe0e343f75234d64cc
Felipe Carvalho <felipecarvalho@gmail.com>
2025-09-12 01:16:16 -0300

    modificaçã: retiranod dependência de layoutStore global./ modificções pontuais de estilização.
    criação: criei função para servir de condicional para renderizar as
    rotas de config ou não/ também serve para aplicar animações.

--- src/layout/LayoutConfig/LayoutConfig.tsx
+++ src/layout/LayoutConfig/LayoutConfig.tsx
@@ -3,18 +3,24 @@ import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Config } from "../../pages/Configuracao/Config";
 import { Button } from "../../component/btn";
 import { Overlay } from "../../component/overlay";
+import { DataConfig } from "../../constants/DataConfig/DataConfig";
 import { useResizeView } from "../../hooks/UseResizeView";
-import { LayoutStore } from "../../store/UseLayout";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { motion } from "framer-motion";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 export function LayoutConfig() {
-  const { setLayout, isLayout } = LayoutStore();
-  const layout = useCallback(() => {
-    return isLayout?.isDesktopLayout ? "flex-row-reverse" : "flex-row";
-  }, [isLayout]);
   const { verificarWidth } = useResizeView();
+  const location = useLocation();
+  const [isLayoutDesktop, setLayoutDesktop] = useState(false);
+
+  const verificarLayoutDesktop = useCallback(() => {
+    return isLayoutDesktop ? "flex-row-reverse" : "flex-row";
+  }, [isLayoutDesktop]);
+
+  const verificarRoute = useCallback(() => {
+    return DataConfig.some((l) => l?.link === location.pathname);
+  }, [location]);
 
   return (
     <Overlay
@@ -24,53 +30,38 @@ export function LayoutConfig() {
       transition={{ type: "spring", stiffness: 150, damping: 15, mass: 1, bounce: 0.3 }}
     >
       {verificarWidth({ largura: 1000 }) && (
-        <div className={`flex h-full w-full items-center !justify-start gap-5 p-5 ${layout()}`}>
+        <div className={`flex h-full w-full items-center !justify-start gap-5 p-5 ${verificarLayoutDesktop()}`}>
           <div className="h-full w-full">
             <Config />
           </div>
           <div className="min-h-max">
-            <Button
-              type="button"
-              onClick={() => setLayout({ isDesktopLayout: !isLayout?.isDesktopLayout, isMobileLayout: isLayout?.isMobileLayout })}
-            >
+            <Button type="button" onClick={() => setLayoutDesktop((s) => !s)}>
               <i>
                 <FontAwesomeIcon icon={faArrowRightArrowLeft} />
               </i>
             </Button>
           </div>
-          <div className="h-full w-full">{<Outlet context={{ setLayout, isLayout }} />}</div>
+          <div className="h-full w-full">{<Outlet />}</div>
         </div>
       )}
       {!verificarWidth({ largura: 1000 }) && (
         <div className="flex h-full w-full items-center !justify-start gap-10 p-5">
           <motion.div
-            initial={{ y: 0 }}
-            animate={isLayout?.isMobileLayout ? { y: -1100 } : { y: 0 }}
+            animate={verificarRoute() ? { y: -1000 } : { y: 0 }}
             transition={{ type: "spring", stiffness: 100 }}
             className="h-full w-full"
           >
             <Config />
           </motion.div>
-          {isLayout?.isMobileLayout && (
-            <motion.div
-              initial={{ y: 1100 }}
-              animate={{ y: 0 }}
-              transition={{ type: "spring", stiffness: 100 }}
-              className="absolute top-5 right-5 bottom-5 left-5 rounded-[50px] bg-white shadow-md shadow-blue-50"
-            >
-              <Outlet context={{ setLayout, isLayout }} />
-            </motion.div>
-          )}
+          <motion.div
+            animate={!verificarRoute() ? { y: 1000 } : { y: 0 }}
+            transition={{ type: "spring", stiffness: 100 }}
+            className="absolute top-5 right-5 bottom-5 left-5 rounded-[50px] !bg-white shadow-md shadow-blue-50"
+          >
+            <Outlet />
+          </motion.div>
         </div>
       )}
     </Overlay>
   );
 }
-
-/*
- try {
-      const layoutStorage = localStorage.getItem("isLayout");
-      return JSON.parse(layoutStorage);
-    } catch (e) {
-      return { isMobileLayout: false, isDesktopLayout: false };
-    } */
