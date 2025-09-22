0d71a346851b9b5869d2e8fa6abda5bb7100baf0
Felipe Carvalho <felipecarvalho@gmail.com>
2025-09-22 20:38:05 -0300

    MODIFICAÇÃO: mudanças de estilo leves.

--- src/pages/Lixeira/Lixeira.tsx
+++ src/pages/Lixeira/Lixeira.tsx
@@ -11,7 +11,7 @@ export function Lixeira() {
   const { lixeira, restaurarTask } = RotinaStore();
 
   return (
-    <div className="z-50 h-full rounded-[50px] bg-blue-50 pb-5 shadow-sm shadow-blue-50">
+    <div className="scrollbar-hide z-50 h-full w-full rounded-[50px] bg-blue-50 pb-5 shadow-sm shadow-blue-50">
       <HeaderContent
         title="Lixeira"
         iconBack={faAngleLeft}
@@ -23,14 +23,14 @@ export function Lixeira() {
         classNameBtn="bg-white !text-blue-400"
         classNameBtnClosed="!min-w-0 !min-h-0 bg-transparent"
       />
-      <div className="scroll-hide h-full overflow-auto rounded-[50px] px-3.5 pt-22">
+      <div className="scrollbar-hide h-full overflow-auto rounded-t-[50px] pt-22">
         {lixeira.length > 0 ? (
           lixeira.map((l) => (
             <div
               key={l?.id}
-              className="mx-1.5 my-3 flex flex-row items-center justify-between rounded-2xl bg-white px-3 py-5 shadow-2xl shadow-blue-100"
+              className="text-shadow-2xl mx-5 my-3 flex flex-row justify-between rounded-2xl bg-white py-5 shadow-blue-100"
             >
-              <div className="item-center flex flex-col justify-between gap-2">
+              <div className="flex flex-col justify-between gap-2 pl-3">
                 <div className="flex gap-0.5">
                   <H3 title="Rotina:" className="text-blue-400" />
                   <P title={l?.rotina} className="xs:max-2xs:w-16 3xs:max-4xs:w-24 truncate text-blue-300 md:w-60" />
@@ -40,7 +40,7 @@ export function Lixeira() {
                   <P title={l?.categoriaID} className="xs:max-2xs:w-10 3xs:max-4xs:w-20 truncate text-blue-300 md:w-60" />
                 </div>
               </div>
-              <div>
+              <div className="pr-3">
                 <Button type="button" onClick={() => restaurarTask(l?.id, l?.categoriaID)}>
                   <p>Restaurar</p>
                 </Button>
