887460bbb7e2564d0dd58ae9b5492803392262e0
Felipe Carvalho <felipecarvalho@gmail.com>
2025-10-14 01:03:30 -0300

    Modificação: ajustes de usabilidade para ver senha ou não, ver erros de validação, e tambem um navLink para rota de criarConta.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useCallback, useState } from "react";
 import { NavLink, useNavigate } from "react-router";
@@ -108,91 +108,104 @@ export function Login() {
   console.log({ formErrorLogin, formSuccessLogin, message, users, tasks });
 
   return (
-    <div className="flex min-h-lvh items-center justify-center gap-10 px-5 md:justify-evenly md:px-10 lg:px-52">
-      {verificarWidth({ largura: 750 }) && (
-        <div className="flex flex-col items-center justify-center gap-5 rounded-2xl md:w-full">
-          <div className="rounded-4xl transition">
-            <img className="h-60 w-60 rounded-4xl" loading="lazy" src="assets/Login.svg" alt="Login" />
-          </div>
-          <div className="p-5">
-            <H3 title="Ainda não tem sua conta?!" className="my-1 !text-[17px] tracking-wide text-blue-400" />
-            <H3
-              title="Crie uma agora mesmo clicando no botão abaixo."
-              className="my-1 !text-[16px] tracking-wide text-blue-300"
-            />
-          </div>
-          <div>
-            <Button type="button" onClick={() => navigate("/criar-conta")}>
-              <p className="text-white">Abrir Conta!</p>
-            </Button>
-          </div>
-        </div>
-      )}
-      <div className="flex-col items-center justify-center gap-2 rounded-4xl border border-blue-50 p-2 shadow-2xl shadow-blue-50 md:w-full min-md:py-4">
-        <div className="text-center">
-          <H1 title="Login" className="font-semibold text-blue-400 sm:text-3xl" />
-        </div>
-        <div className="w-full rounded-4xl p-4 md:p-2">
-          <form onSubmit={handleSubmitLogin} className="flex flex-col gap-5">
-            <label className="flex flex-col items-start gap-1.5">
-              <div className="flex w-full flex-col items-start">
-                <P title="Usuario:" className="text-blue-400" />
-                <Input
-                  onChange={handleLoginAccount}
-                  onBlur={() => verificarInputs({ inputName: "user" })}
-                  name="user"
-                  type="text"
-                  placeholder="Digite seu nome de Usuário"
-                  className="bg-white !shadow-none"
-                />
-              </div>
-              <P
-                title={`${formErrorLogin?.user.length > 0 ? formErrorLogin?.user : message?.error?.user}`}
-                className="text-xs font-medium text-red-400"
+    <div className="flex min-h-lvh flex-col items-center justify-center gap-10">
+      <div className="flex items-center justify-center gap-10 px-5 md:justify-evenly md:px-10 lg:px-52">
+        {verificarWidth({ largura: 750 }) && (
+          <div className="flex flex-col items-center justify-center gap-5 rounded-2xl md:w-full">
+            <div className="rounded-4xl transition">
+              <img className="h-60 w-60 rounded-4xl" loading="lazy" src="assets/Login.svg" alt="Login" />
+            </div>
+            <div className="p-5">
+              <H3 title="Ainda não tem sua conta?!" className="my-1 !text-[17px] tracking-wide text-blue-400" />
+              <H3
+                title="Crie uma agora mesmo clicando no botão abaixo."
+                className="my-1 !text-[16px] tracking-wide text-blue-300"
               />
-            </label>
-            <label className="flex flex-col items-start gap-1.5">
-              <P title="Senha:" className="text-blue-400" />
-              <div className="flex w-full flex-row items-center justify-center gap-2">
-                <Input
-                  onChange={handleLoginAccount}
-                  onBlur={() => verificarInputs({ inputName: "password" })}
-                  name="password"
-                  type={typeInput ? "password" : "text"}
-                  placeholder="Digite seus senha"
-                  className="bg-white !shadow-none"
-                />
-                <Button
-                  type="button"
-                  className="!min-h-11 !min-w-11 !bg-white !p-0 !text-blue-400"
-                  onClick={() => setType((s) => !s)}
-                >
-                  <i>
-                    <FontAwesomeIcon icon={typeInput ? faEye : faEyeSlash} />
-                  </i>
-                </Button>
-              </div>
-              <P
-                title={`${formErrorLogin?.password.length > 0 ? formErrorLogin?.password : message?.error?.password}`}
-                className="text-xs font-medium text-red-400"
-              />
-            </label>
-            <div className="flex w-full flex-row items-center justify-center gap-5">
-              <Button type="submit">
-                <p className="font-medium">Confirmar</p>
-              </Button>
-              <Button onClick={() => formClear()} type="reset" className="!bg-white !text-blue-400">
-                <p className="font-medium">Cancelar</p>
+            </div>
+            <div>
+              <Button type="button" onClick={() => navigate("/criar-conta")}>
+                <p className="text-white">Abrir Conta!</p>
               </Button>
             </div>
-          </form>
-        </div>
-        <div className="w-full text-end">
-          <NavLink to="/redefinir-senha">
-            <P title="Esqueceu a senha?" className="text-[13px] text-blue-300" />
-          </NavLink>
+          </div>
+        )}
+        <div className="flex-col items-center justify-center gap-2 rounded-4xl border border-blue-50 p-2 shadow-2xl shadow-blue-50 md:w-full min-md:py-4">
+          <div className="text-center">
+            <H1 title="Login" className="font-semibold text-blue-400 sm:text-3xl" />
+          </div>
+          <div className="w-full rounded-4xl p-4 md:p-2">
+            <form onSubmit={handleSubmitLogin} className="flex flex-col gap-5">
+              <label className="flex flex-col items-start gap-1.5">
+                <div className="flex w-full flex-col items-start">
+                  <P title="Usuario:" className="text-blue-400" />
+                  <Input
+                    onChange={handleLoginAccount}
+                    onBlur={() => verificarInputs({ inputName: "user" })}
+                    name="user"
+                    type="text"
+                    placeholder="Digite seu nome de Usuário"
+                    className="bg-white !shadow-none"
+                  />
+                </div>
+                <P
+                  title={`${formErrorLogin?.user.length > 0 ? formErrorLogin?.user : message?.error?.user}`}
+                  className="text-xs font-medium text-red-400"
+                />
+              </label>
+              <label className="flex flex-col items-start gap-1.5">
+                <P title="Senha:" className="text-blue-400" />
+                <div className="flex w-full flex-row items-center justify-center gap-2">
+                  <Input
+                    onChange={handleLoginAccount}
+                    onBlur={() => verificarInputs({ inputName: "password" })}
+                    name="password"
+                    type={typeInput ? "password" : "text"}
+                    placeholder="Digite seus senha"
+                    className="bg-white !shadow-none"
+                  />
+                  <Button
+                    type="button"
+                    className="!min-h-11 !min-w-11 !bg-white !p-0 !text-blue-400"
+                    onClick={() => setType((s) => !s)}
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={typeInput ? faEye : faEyeSlash} />
+                    </i>
+                  </Button>
+                </div>
+                <P
+                  title={`${formErrorLogin?.password.length > 0 ? formErrorLogin?.password : message?.error?.password}`}
+                  className="text-xs font-medium text-red-400"
+                />
+              </label>
+              <div className="flex w-full flex-row items-center justify-center gap-5">
+                <Button type="submit">
+                  <p className="font-medium">Confirmar</p>
+                </Button>
+                <Button onClick={() => formClear()} type="reset" className="!bg-white !text-blue-400">
+                  <p className="font-medium">Cancelar</p>
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div className="w-full text-end">
+            <NavLink to="/redefinir-senha">
+              <P title="Esqueceu a senha?" className="text-[13px] text-blue-300" />
+            </NavLink>
+          </div>
         </div>
       </div>
+      {!verificarWidth({ largura: 750 }) && (
+        <NavLink
+          to="/criar-conta"
+          className="flex flex-row items-center justify-center gap-2 text-blue-400 underline underline-offset-4"
+        >
+          <P title="Abrir conta" className="font-medium" />
+          <i>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </i>
+        </NavLink>
+      )}
     </div>
   );
 }
