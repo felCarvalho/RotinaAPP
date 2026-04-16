9449f206237fa443553c731d7599dbaf4868e669
felipe <felipecarvalho2934@gmail.com>
2026-04-16 20:00:11 -0300

    feat: aplica LayoutHome como wrapper global e corrige navegação com useNavigate

--- app/layout/LayoutHome/layout.tsx
+++ app/layout/LayoutHome/layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet, redirect } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { Button } from "../../component/btn";
 
 export default function HomeLayout() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex h-full w-full flex-row">
       <div>
-        <Button type="button" onClick={() => redirect("/home")}>
+        <Button type="button" onClick={() => navigate("/home")}>
           Aperte para ir para tela de home
         </Button>
       </div>
