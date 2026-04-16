eea9e95ad4077e32ac55fbd2bf1cb22c94420a83
felipe <felipecarvalho2934@gmail.com>
2026-04-16 19:56:03 -0300

    refactor: move middleware para LayoutRegister e altera estrutura de LayoutHome

--- app/layout/LayoutHome/layout.tsx
+++ app/layout/LayoutHome/layout.tsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router";
-import { Header } from "../Header/Header";
-import { middleware } from "../../middleware/middleware.server";
-export { middleware };
+import { Outlet, redirect } from "react-router";
+import { Button } from "../../component/btn";
 
 export default function HomeLayout() {
   return (
     <div className="flex h-full w-full flex-row">
-      <Header />
+      <div>
+        <Button type="button" onClick={() => redirect("/home")}>
+          Aperte para ir para tela de home
+        </Button>
+      </div>
       <main className="w-full">
         <Outlet />
       </main>
