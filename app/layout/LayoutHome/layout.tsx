84814c1ccd087378ff4f9c4c59eafb0805baf13c
felipe <felipecarvalho2934@gmail.com>
2026-04-17 13:44:06 -0300

    feat: add Header component to HomeLayout

--- app/layout/LayoutHome/layout.tsx
+++ app/layout/LayoutHome/layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import { Header } from "../Header/Header";
 
 export default function HomeLayout() {
   return (
     <div className="flex h-full w-full flex-row">
+      <Header />
       <main className="w-full">
         <Outlet />
       </main>
